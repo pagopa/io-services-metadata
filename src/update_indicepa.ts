49c44e3e37b558cdbb0fa062f589111c9fbd9dd1
Federico Feroldi <federico@teamdigitale.governo.it>
2018-07-26 18:01:37 +0200

    Implements logic that downloads metadata and coverts them to json

---
+++ src/update_indicepa.ts
@@ -0,0 +1,39 @@
+import * as request from "request";
+import * as es from "event-stream";
+import * as fse from "fs-extra";
+import { createStream } from "csv-stream";
+
+import { AmministrazioneIPA } from "./types";
+
+const INDICEPA_URL = "https://www.indicepa.gov.it/public-services/opendata-read-service.php?dstype=FS&filename=amministrazioni.txt";
+
+const OUT_DIR = `${__dirname}/../amministrazioni`;
+
+console.info("Retrieving indicepa data");
+
+const csvStream = createStream({
+  delimiter : '\t'
+});
+
+request(INDICEPA_URL) // fetch data from remote URL
+  .pipe(csvStream) // parse CSV
+  .pipe(es.map((entry: AmministrazioneIPA, cb) => {
+    if(entry.cf_validato !== "S") {
+      // filter out entries without a validated CF
+      cb();
+    }
+
+    // strip leading 0s from CF
+    const entryCf = entry.Cf.replace(/^0+/, "");
+
+    if(entryCf.length < 2 || !entryCf.match(/^\d+$/)) {
+      // filter out entries with bogus CF
+      cb();
+    }
+
+    const fullPath = `${OUT_DIR}/${entryCf[0]}${entryCf[1]}/${entryCf}.json`;
+
+    console.log(`Writing ${fullPath}`);
+    return fse.outputJson(fullPath, entry, cb);
+  }))
+  .on("error", console.error);

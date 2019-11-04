2142f0f2f118b7bd1f7f22dfbeeaaf5c441efd90
Matteo Boschi <mbmaken@gmail.com>
2019-11-04 18:28:11 +0100

    [#169314298] add scope service generation and dump

--- src/update_services.ts
+++ src/update_services.ts
@@ -7,9 +7,10 @@ import { PathReporter } from "io-ts/lib/PathReporter";
 import * as yaml from "js-yaml";
 import * as path from "path";
 
-import { Service } from "../definitions/Service";
+import { scopeEnum, Service } from "../definitions/Service";
 
 const Services = t.dictionary(t.string, Service);
+type ScopeServices = { [key in keyof typeof scopeEnum]: ReadonlyArray<string> };
 const root = path.join(__dirname, "../");
 
 async function run(rootPath: string): Promise<void> {
@@ -18,7 +19,7 @@ async function run(rootPath: string): Promise<void> {
   const servicesYamlPath = path.join(rootPath, "services.yml");
   console.log("Services YAML:", chalk.blueBright(servicesYamlPath));
 
-  console.log(chalk.gray("[1/2]"), "Reading services data...");
+  console.log(chalk.gray("[1/3]"), "Reading services data...");
   const servicesYamlContent = await fs.readFile(servicesYamlPath);
   try {
     const servicesYamlData = yaml.safeLoad(servicesYamlContent.toString(), {
@@ -35,7 +36,7 @@ async function run(rootPath: string): Promise<void> {
     const serviceIds = Object.keys(services);
     console.log(chalk.greenBright(`Found ${serviceIds.length} service(s).`));
 
-    console.log(chalk.gray("[2/2]"), "Generating services JSON...");
+    console.log(chalk.gray("[2/3]"), "Generating services JSON...");
     await Promise.all(
       serviceIds.map(async serviceId => {
         const servicePath = path.join(
@@ -49,6 +50,22 @@ async function run(rootPath: string): Promise<void> {
         );
       })
     );
+
+    const locals = serviceIds.filter(sId => {
+      const service = services[sId];
+      return service.scope === scopeEnum.LOCAL;
+    });
+    const nationals = serviceIds.filter(sId => {
+      const service = services[sId];
+      return service.scope === scopeEnum.NATIONAL;
+    });
+    const scopeService: ScopeServices = { NATIONAL: nationals, LOCAL: locals };
+    await fs.writeFile(
+      path.join(root, path.join("services", "scopeServices.json")),
+      JSON.stringify(scopeService)
+    );
+
+    console.log(chalk.gray("[2/3]"), "Generating services JSON...");
   } catch (e) {
     console.log(chalk.red(e.message));
     throw e;

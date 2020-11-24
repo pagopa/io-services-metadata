30bfe710325f9031377c878494850d57fb8e9408
Matteo Boschi <mbmaken@gmail.com>
2020-11-24 12:25:59 +0100

    Bonus and Contextual data type guards (#154)
    * add type guards and check steps in circeci
    
    * wip

---
+++ src/checkContextualHelpData.ts
@@ -0,0 +1,16 @@
+// a simple check that bonus available json respects type definition
+
+import * as fs from "fs";
+import { ContextualHelp } from "../definitions/ContextualHelp";
+
+const isRight = ContextualHelp.decode(
+  JSON.parse(
+    fs.readFileSync(__dirname + "/../contextualhelp/data.json").toString()
+  )
+).isRight();
+if (!isRight) {
+  console.error(
+    "contextualhelp/data.json is not compatible with ContextualHelp type"
+  );
+}
+process.exit(isRight ? 0 : 1);

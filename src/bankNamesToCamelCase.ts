1a2c5c39e6ec926a964d3fc4b71c89911923b7ef
Giovanni Mancini <g.mancini.0gmail.com>
2020-12-11 12:46:32 +0100

    [#175764810] Better immutability

--- src/bankNamesToCamelCase.ts
+++ src/bankNamesToCamelCase.ts
@@ -2,10 +2,15 @@ import { capitalCase } from "change-case";
 import { debug as cdebug } from "console";
 import { readFileSync } from "fs";
 
+type Bank = { abi: number; name: string };
+// const bcc = /(B C C) | (Banca Di Credito Cooperativo)/;
+
 const statusAbi = JSON.parse(readFileSync(__dirname + "/../status/abi.json").toString());
+const banks : Array<Bank> = statusAbi.data;
+const bankNameReducer = (accumulator: Array<Bank>, currentBank: Bank) => (
+  accumulator.concat({ abi: currentBank.abi, name: capitalCase(currentBank.name) } as Bank));
+const updBanks = banks.reduce(bankNameReducer, []);
+const updStatusAbi = {data: updBanks};
 
-statusAbi.data.map((bank: { abi: number; name: string}) => { 
-  bank.name = capitalCase(bank.name);
-});
-
-cdebug("JSON", statusAbi);
+cdebug("JSON", JSON.stringify(updStatusAbi));
+cdebug("OBJ", updStatusAbi);

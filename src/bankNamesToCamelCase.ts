545454a7fe3523034dc5233f356b6dce36d99c69
Giovanni Mancini <g.mancini.0gmail.com>
2020-12-11 12:04:13 +0100

    [#175764810] Fixed immutability to comply with linter

--- src/bankNamesToCamelCase.ts
+++ src/bankNamesToCamelCase.ts
@@ -1,8 +1,8 @@
+import { capitalCase } from "change-case";
 import { debug as cdebug } from "console";
 import { readFileSync } from "fs";
-import { capitalCase } from "change-case";
 
-let statusAbi = JSON.parse(readFileSync(__dirname + "/../status/abi.json").toString());
+const statusAbi = JSON.parse(readFileSync(__dirname + "/../status/abi.json").toString());
 
 statusAbi.data.map((bank: { abi: number; name: string}) => { 
   bank.name = capitalCase(bank.name);

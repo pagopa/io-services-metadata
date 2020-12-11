58f6ce0b2360400089b84d9ac26878780f2d97bc
Giovanni Mancini <g.mancini.0gmail.com>
2020-12-11 11:44:54 +0100

    [#175764810] Added script logic

--- src/bankNamesToCamelCase.ts
+++ src/bankNamesToCamelCase.ts
@@ -1,5 +1,11 @@
 import { debug as cdebug } from "console";
 import { readFileSync } from "fs";
+import { capitalCase } from "change-case";
 
-const myFile = readFileSync(__dirname + "/../status/abi.json");
-cdebug("FILE", myFile);
+let statusAbi = JSON.parse(readFileSync(__dirname + "/../status/abi.json").toString());
+
+statusAbi.data.map((bank: { abi: number; name: string}) => { 
+  bank.name = capitalCase(bank.name);
+});
+
+cdebug("JSON", statusAbi);

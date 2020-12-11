cc22bea2d107669050edae9131d5fee2db56a9f5
Giovanni Mancini <g.mancini.0gmail.com>
2020-12-11 13:23:53 +0100

    [#175764810] Fixed BCC and SPA

--- src/bankNamesToCamelCase.ts
+++ src/bankNamesToCamelCase.ts
@@ -7,6 +7,9 @@ interface IBank {
   name: string;
 }
 
+const bccR = /(B C C)|(Bcc)/;
+const spaR = /(S P A)|(Spa)/;
+
 const statusAbi = JSON.parse(
   readFileSync(__dirname + "/../status/abi.json").toString()
 );
@@ -15,11 +18,15 @@ const banks = statusAbi.data as ReadonlyArray<IBank>;
 const bankNameReducer = (
   accumulator: ReadonlyArray<IBank>,
   currentBank: IBank
-) =>
-  accumulator.concat({
+) => {
+  const ccBankname = capitalCase(currentBank.name)
+    .replace(bccR, "BCC")
+    .replace(spaR, "SPA");
+  return accumulator.concat({
     abi: currentBank.abi,
-    name: capitalCase(currentBank.name)
+    name: ccBankname
   } as IBank);
+};
 const updBanks = banks.reduce(bankNameReducer, []);
 const updStatusAbi = { data: updBanks };
 

4014131abdce9fcc998bf02cb5a6fff59aff499d
Giovanni Mancini <g.mancini.0gmail.com>
2021-01-25 10:18:21 +0100

    [#175764810] Modified src and dst file

--- src/bankNamesToCamelCase.ts
+++ src/bankNamesToCamelCase.ts
@@ -3,10 +3,7 @@ import { readFileSync, writeFileSync } from "fs";
 import { toCamelCase, IAbi } from "./utils/bankNames";
 
 const updStatusAbi = toCamelCase(JSON.parse(
-  readFileSync(__dirname + "/../status/abi.json").toString()
+  readFileSync(__dirname + "/../bonus/bpd/abi/pm_abi.json").toString()
 ) as IAbi);
 
-writeFileSync(
-  __dirname + "/../status/abiCapitalCase.json",
-  JSON.stringify(updStatusAbi)
-);
+writeFileSync(__dirname + "/../status/abi.json", JSON.stringify(updStatusAbi));

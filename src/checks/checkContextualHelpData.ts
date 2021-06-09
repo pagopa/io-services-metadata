6f9a0e806ba52e191ade51150ad2534228459e65
Matteo Boschi <mbmaken@gmail.com>
2021-06-09 19:36:27 +0200

    improve contextual help data check

--- src/checks/checkContextualHelpData.ts
+++ src/checks/checkContextualHelpData.ts
@@ -2,15 +2,40 @@
 
 import * as fs from "fs";
 import { ContextualHelp } from "../../generated/definitions/content/ContextualHelp";
+import { getDuplicates } from "../utils/collections";
 
-const isRight = ContextualHelp.decode(
+const error = (message: string) => {
+  console.error(message);
+  process.exit(1);
+};
+
+const contextualHelpData = ContextualHelp.decode(
   JSON.parse(
     fs.readFileSync(__dirname + "/../../contextualhelp/data.json").toString()
   )
-).isRight();
-if (!isRight) {
-  console.error(
-    "contextualhelp/data.json is not compatible with ContextualHelp type"
+);
+if (!contextualHelpData.isRight()) {
+  error("contextualhelp/data.json is not compatible with ContextualHelp type");
+} else {
+  // check for duplicates
+  const itScreens = getDuplicates(
+    contextualHelpData.value.it.screens,
+    (a, b) => a.route_name === b.route_name
   );
+  const enScreens = getDuplicates(
+    contextualHelpData.value.en.screens,
+    (a, b) => a.route_name === b.route_name
+  );
+  if (itScreens.length + enScreens.length > 0) {
+    error(
+      `these screens are repeated more than one time :\n${[
+        ...itScreens,
+        ...enScreens
+      ]
+        .map(d => d.route_name)
+        .join("\n")}`
+    );
+  }
 }
-process.exit(isRight ? 0 : 1);
+
+process.exit(0);

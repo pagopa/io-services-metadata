562b95f3970545a6357115ea811583702ee886e6
Simone <simone.biffi.91@gmail.com>
2021-12-10 11:07:51 +0100

    [IA-535] Add Zendesk config (#526)
    * add zendesk definition
    
    * add zendesk config data
    
    * add zendesk check
    
    * move zendesk config to assistanceTools dir
    
    * modify file path
    
    * update definition
    
    * fix optional field
    
    * review
    
    Co-authored-by: Matteo Boschi <mbmaken@gmail.com>

---
+++ src/checks/checkZendesk.ts
@@ -0,0 +1,46 @@
+import fs from "fs";
+import { Zendesk } from "../../generated/definitions/content/Zendesk";
+import { ZendeskCategory } from "../../generated/definitions/content/ZendeskCategory";
+import { getDuplicates } from "../utils/collections";
+
+/**
+ * this script checks the zendesk config
+ * it ensures that the json data match the expected type the app receives
+ */
+
+const error = (message: string) => {
+  console.error(message);
+  process.exit(1);
+};
+
+const fileContent = fs
+  .readFileSync(__dirname + "/../../assistenceTools/zendesk.json")
+  .toString();
+const maybeZendeskConfig = Zendesk.decode(JSON.parse(fileContent));
+
+if (maybeZendeskConfig.isLeft()) {
+  error(`can't decode zendesk config assistenceTools/zendesk.json`);
+} else {
+  if (
+    maybeZendeskConfig.value.zendeskCategories &&
+    maybeZendeskConfig.value.zendeskCategories.categories.length === 0
+  ) {
+    error(`The categories field can't be an empty array`);
+  }
+
+  if (maybeZendeskConfig.value.zendeskCategories) {
+    const zendeskCategories = maybeZendeskConfig.value.zendeskCategories;
+    // check for duplicates in categories
+    const duplicatedCategoriesValue = getDuplicates(
+      zendeskCategories.categories,
+      (a: ZendeskCategory, b: ZendeskCategory) => a.value === b.value
+    );
+    if (duplicatedCategoriesValue.length > 0) {
+      error(
+        `these categories are repeated more than one time:\n${duplicatedCategoriesValue
+          .map(d => d.value)
+          .join("\n")}`
+      );
+    }
+  }
+}
